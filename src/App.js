592cfc5f09023c1531a48509078266a076722175
FrankieBortot <bortot.frankie@gmail.com>
2024-07-10 11:32:46 +0200

    cards

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
 import './App.css';
+import { articleList } from './articles.js';
 
 function App() {
+  let link = articleList[0];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Hero />
+      <Card article={link} />
     </div>
+  )
+}
+
+function Hero() {
+  return (
+    <div>
+      <h1>
+      My favorite things
+      </h1>   
+    </div>
+  )
+}
+
+function Card({ article }) {
+
+  const listTags = article.tags.map(
+    tag => 
+    <Tag key={tag} text={tag}/>
   );
+
+  return (
+  <div className='card-article'>
+    <div className='card-image' >
+    <img src={article.image} alt="Girl in a jacket" />
+    </div>
+    <div className='body'>
+      <div className='title'>
+        {article.title}
+      </div>
+      <div className='description'>
+        {article.description}
+      </div>
+
+      <div className='tag-list'>
+        {listTags}
+      </div>
+
+      
+
+    </div>
+  </div>
+  )
+}
+
+function Tag({text}) {
+
+  return (
+    <div className='tag'>
+      {text}
+    </div>
+  )
 }
 
 export default App;
---
+++ src/articles.js
@@ -0,0 +1,21 @@
+
+export const articleList = [{
+
+    title : "How to define color usage through semantic sets for design systems",
+    author : "Katie Cooper",
+    image: "https://miro.medium.com/v2/resize:fit:1400/format:webp/1*Ne878-xcSVCBDL9wMMCrsw.png",
+    link: "https://uxdesign.cc/how-to-define-color-usage-through-semantic-sets-for-design-systems-99445804233d",
+    
+    description : "descrizione messa a mano da me >:)",
+    
+    tags : [
+    "design system",
+    "tokens",
+    "colors",
+    "figma"
+    ]
+    
+    }
+]
+
+    
